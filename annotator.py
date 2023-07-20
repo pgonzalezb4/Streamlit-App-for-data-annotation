c286bb75a967e8f29d426383d9ec8e821bf9ef86
pgonzalezb4 <pablo.gonzalez@mercadolibre.com.co>
2023-07-20 11:48:55 -0500

    fix both forms

--- annotator.py
+++ annotator.py
@@ -4,7 +4,6 @@ import streamlit as st
 dataset = pd.read_parquet('master_news_scraped.parquet.gzip')
 
 factor_labels = [
-    "No Factor",
     "Factor 1: Constraints on Government Powers",
     "Factor 2: Absence of Corruption",
     "Factor 3: Open Government",
@@ -19,14 +18,22 @@ sentiment_labels = [
     "Positive",
     "Neutral",
     "Negative",
-    "No Factor"
 ]
 
 if 'idx' not in st.session_state:
     st.session_state.idx = 0
 
+if 'stage' not in st.session_state:
+    st.session_state.stage = 0
+
+if 'show_second_form' not in st.session_state:
+    st.session_state.show_second_form = False
+
+if 'submitted_second_form' not in st.session_state:
+    st.session_state.submitted_second_form = False
+
 if 'annotations' not in st.session_state:
-    st.session_state.annotations = pd.DataFrame({'url': [], 'factor': [], 'sentiment': []})
+    st.session_state.annotations = pd.DataFrame({'url': [], 'factor(s)': [], 'sentiment': []})
 
 if 'available_factor_labels' not in st.session_state:
     st.session_state.available_factor_labels = factor_labels
@@ -42,41 +49,77 @@ def convert_df_to_csv(df):
 def show_sample(streamlit_placeholder, sample_idx):
     if sample_idx < len(dataset):
         with streamlit_placeholder.container():
-            st.markdown(f"Country: `{dataset.iloc[sample_idx]['country']}`")
-            st.markdown(f"Source: `{dataset.iloc[sample_idx]['source']}`")
-            st.markdown(f"URL: `{dataset.iloc[sample_idx]['url']}`")
-            st.markdown(f"News title: `{dataset.iloc[sample_idx]['title_eng']}`")
-            st.markdown(f"News summary: `{dataset.iloc[sample_idx]['desc_eng']}`")
+            st.markdown("""
+            <style>
+            .title {
+                font-size:30px !important;
+            }
+            .subtitle {
+                font-size:18px !important;
+            }
+            .url_btn {
+                background-color:#ECECEC;
+                padding:10px;
+                border-radius:5px;
+                text-decoration:none;
+            }
+            </style>
+            """, unsafe_allow_html=True)
+            st.markdown(f"<a style='color:#000000;' class='url_btn' target='_blank' href='{dataset.iloc[sample_idx]['url']}'>News source</a>", unsafe_allow_html=True)
+            st.markdown(f"<b class='title'>{dataset.iloc[sample_idx]['country']}</b>", unsafe_allow_html=True)
+            st.markdown(f"<b class='title'>{dataset.iloc[sample_idx]['title_eng']}</b>", unsafe_allow_html=True)
+            st.markdown(f"<p class='subtitle'>{dataset.iloc[sample_idx]['desc_eng']}</p>", unsafe_allow_html=True)
 
 def set_label(label, sample_idx):
     if sample_idx < len(dataset):
         st.session_state.annotations = pd.concat([st.session_state.annotations, 
                                                   pd.DataFrame({'url': [dataset.iloc[sample_idx]['url']], 'factor': [label[0]], 'sentiment': [label[1]]})], 
                                                  ignore_index=True)
-
-with st.form('labeling', clear_on_submit=True):
-
+        
+def set_stage(stage):
+    st.session_state.stage = stage
+        
+first_form = st.empty()
+second_form = st.empty()
+with first_form.form("isrelated"):
     data_container = st.empty()
-
     show_sample(data_container, st.session_state.idx)
-    selected_factor_label = st.radio("Select to which factor this news text belongs:", sorted(st.session_state.available_factor_labels))
-    selected_sentiment_label = st.radio("Select change in Rule of Law:", sorted(st.session_state.available_sentiment_labels))
 
-    submit_col = st.columns(1)
-    submit = submit_col[0].form_submit_button("Submit")
+    isrelated = st.radio('Is this news text related to the Rule of Law?', ['Yes', 'No'])
+    st.form_submit_button("Submit", on_click=set_stage, args=(1,))
 
-    if submit:
-        set_label([selected_factor_label, selected_sentiment_label], st.session_state.idx)
-        st.session_state.idx += 1
+    if st.session_state.stage > 0:
+        if isrelated == 'Yes':
+            with second_form.form('labeling'):
+                selected_factor_label = st.multiselect("Select to which factor(s) this news text belongs **(if you select multiple, select them in order of relevance)**:", sorted(st.session_state.available_factor_labels))
+                selected_sentiment_label = st.radio("Select change in Rule of Law:", sorted(st.session_state.available_sentiment_labels))
+                st.form_submit_button(label="Submit selection of factor and sentiment", on_click=set_stage, args=(2,))
+
+                if st.session_state.stage > 1:
+                    print(st.session_state.stage)
+                    set_label([str(selected_factor_label), selected_sentiment_label], st.session_state.idx)
+                    second_form.empty()
+                    st.session_state.idx += 1
+
+        elif isrelated == 'No':
+            set_label(['Not related', 'Not related'], st.session_state.idx)
+            st.session_state.idx += 1
+
         show_sample(data_container, st.session_state.idx)
         if st.session_state.idx >= len(dataset):
             data_container.text("No more data to annotate")
+    
+        st.session_state.stage = 0
+
 
 st.info(f"Annotated: {len(st.session_state.annotations)}")
+
 st.download_button(
-    "Download annotations as CSV file",
-    data=convert_df_to_csv(st.session_state.annotations),
-    file_name='annotations.csv',
-    mime='text/csv',
-)
-st.write(st.session_state.annotations)
+        "Download annotations as CSV file",
+        data=convert_df_to_csv(st.session_state.annotations),
+        file_name='annotations.csv',
+        mime='text/csv',
+    )
+
+with st.expander("Click here to see annotations"):
+    st.write(st.session_state.annotations)
